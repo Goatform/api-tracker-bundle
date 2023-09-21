3f4281a2c914fb161a586af22ae4ffbb4efc1aaa
Goatform <8354705+Goatform@users.noreply.github.com>
2023-09-21 10:55:50 +0200

    Added test structure for this project

---
+++ src/DependencyInjection/ApiTrackerBundleExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Goatform\ApiTracker\Bundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ApiTrackerBundleExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = $this->getConfiguration($configs, $container);
+        $config        = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+        $loader->load('goatform_api_tracker_bundle.yaml');
+
+        $container->setParameter('goatform_api_tracker_bundle.api_url', $config['api_url']);
+    }
+
+    public function getAlias(): string
+    {
+        return 'goatform_api_tracker';
+    }
+
+    public function getConfiguration(array $config, ContainerBuilder $container): Configuration
+    {
+        return new Configuration($this->getAlias());
+    }
+}
